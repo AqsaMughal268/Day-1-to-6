4668c481c5829d20800fa04d6c844db3678ec6ea
AqsaMughal268 <165940283+AqsaMughal268@users.noreply.github.com>
2025-02-04 10:37:18 -0800

    build

--- src/app/components/4BestAirmax.tsx
+++ src/app/components/4BestAirmax.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import { client } from "@/sanity/lib/client";
-import { url } from "inspector";
+
 import { urlFor } from "@/sanity/lib/image";
 import Image  from "next/image";
 import React, { useEffect, useState } from "react";
